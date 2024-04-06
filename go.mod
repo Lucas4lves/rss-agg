cafb02bcc6dbc2078c045ecce2ec600a16451740
lucas4lves <zombnfl@gmail.com>
2024-04-05 21:51:08 -0300

    Chore: initial files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lucas4lves/rss-agg
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	authorName := "Lucas"
+
+	fmt.Printf("RSS Aggregator | by: %s\n", authorName)
+}
